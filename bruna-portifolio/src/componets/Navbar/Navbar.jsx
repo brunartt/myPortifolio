c335a3580caa39e886b576ace050d32c097ab5c1
Bruna Roberta <144182969+brunartt@users.noreply.github.com>
2024-08-28 15:46:48 -0300

    Implementação completa da NavBar

--- bruna-portifolio/src/componets/Navbar/Navbar.jsx
+++ bruna-portifolio/src/componets/Navbar/Navbar.jsx
@@ -1,26 +1,32 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import styles from "./Navbar.module.css";
 import { getImageUrl } from "../../utils";
 
 export const NavBar = () => {
-    return <nav className = {styles.navbar}>
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    return <nav className={styles.navbar}>
         <><a className={styles.title} href="/">Portifólio</a>
-        <div className={styles.menu}>
-            <img className={styles.menuBtn} src={getImageUrl()}/>
-            <ul className={styles.menuItems}>
-                <li>
-                    <a href="#about">Sobre</a>
-                </li>
-                <li>
-                    <a href="#experience">Experiência</a>
-                </li>
-                <li>
-                    <a href="#project">Projetos</a>
-                </li>
-                <li>
-                    <a href="#contact">Contato</a>
-                </li>
-            </ul>
-        </div></>
+            <div className={styles.menu}>
+                <img className={styles.menuBtn} 
+                     src={menuOpen ? getImageUrl("nav/closeIcon.png") : getImageUrl("nav/menuIcon.png")} 
+                     alt="menu-button" onClick={() => setMenuOpen(!menuOpen)}/>
+                     
+                <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+                onClick={() => setMenuOpen(false)}>
+                    <li>
+                        <a href="#about">Sobre</a>
+                    </li>
+                    <li>
+                        <a href="#experience">Experiência</a>
+                    </li>
+                    <li>
+                        <a href="#project">Projetos</a>
+                    </li>
+                    <li>
+                        <a href="#contact">Contato</a>
+                    </li>
+                </ul>
+            </div></>
     </nav>
 }
